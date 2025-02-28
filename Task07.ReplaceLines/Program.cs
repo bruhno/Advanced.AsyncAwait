8a22f0304a0f4758b4a1e756a1cabd100212737c
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-02-28 13:06:50 +0300

    Add project files.

---
+++ Task07.ReplaceLines/Program.cs
@@ -0,0 +1,15 @@
+﻿//How to replace lines 10 and 11 so that this code runs concurrently
+
+Func<int, Task> process = async x =>
+{
+    await Task.Delay(1000); //Stub for some async operation.
+    Console.WriteLine($"End processing {x}");
+};
+
+int[] array = [1, 2, 3, 4, 5];
+var tasks = array.Select(a => process(a));
+await Task.WhenAll([.. tasks]);
+//foreach (int i in array)
+//    await process(i);
+
+Console.WriteLine("All items processed");

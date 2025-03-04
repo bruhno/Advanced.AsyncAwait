7c9d204730a5093b557c836b3734fe72627a411d
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-03-04 11:07:41 +0300

    use Unwrap()

---
+++ Task08.OldAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿var cts = new CancellationTokenSource();
+
+var t = Task.Factory.StartNew(() => ExecuteAsync(cts.Token), TaskCreationOptions.LongRunning);
+
+var ss = Task.Run(() => 1);
+
+await t.Unwrap();
+
+async Task ExecuteAsync(CancellationToken ct)
+{
+    while (!ct.IsCancellationRequested)
+    {
+        Console.WriteLine($"Do smth");
+        await Task.Delay(1000);
+    }
+}

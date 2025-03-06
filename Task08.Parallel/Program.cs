162ec15d843ed4e094d4a75971ed681e35eed922
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-03-06 07:44:53 +0300

    add Task09

---
+++ Task08.Parallel/Program.cs
@@ -0,0 +1,16 @@
+﻿await Parallel.ForEachAsync(
+    [1, 2, 3], 
+    CancellationToken.None, 
+    ExecuteAsync);
+
+
+async ValueTask ExecuteAsync(int num, CancellationToken cancellationToken)
+{
+    while (true)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        Console.WriteLine($"Do smth {num}");
+        await Task.Delay(1000 + num * 100);
+    }
+}
---
+++ Task09.InCurrentThread/Program.cs
@@ -0,0 +1,19 @@
+﻿var t = CreateTask2();
+
+Thread.Sleep(1000);
+
+Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+await t;
+Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+Task CreateTask1()
+{
+    Thread.Sleep(1000);
+    return Task.CompletedTask;
+}
+
+
+async Task CreateTask2()
+{
+    await Task.Delay(500);
+}

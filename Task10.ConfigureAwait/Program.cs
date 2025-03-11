737e6910b244dd7f6f7f64c29aa52bc990d2bfb7
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-03-11 21:38:17 +0300

    Task11 custom awaiter

--- Task10.ConfigureAwait/Program.cs
+++ Task10.ConfigureAwait/Program.cs
@@ -4,15 +4,24 @@ SynchronizationContext.SetSynchronizationContext(my);
 
 Console.WriteLine($"Begin: {Environment.CurrentManagedThreadId}: SyncCtx: {SynchronizationContext.Current}");
 
-await DoDelay(1000);
+var t = DoDelay(100);
+
+Thread.Sleep(200);
+
+await t;//.ConfigureAwait(ConfigureAwaitOptions.ForceYielding); ;
 
 
 Console.WriteLine($"End: {Environment.CurrentManagedThreadId}: SyncCtx: {SynchronizationContext.Current}");
 
 async Task DoDelay(int millisec)
 {
-    Console.WriteLine($"DoDelay1: SyncCtx: {SynchronizationContext.Current}");
-    await Task.Delay(millisec).ConfigureAwait(false);
+    Console.WriteLine($"DoDelay1: {Environment.CurrentManagedThreadId}: SyncCtx: {SynchronizationContext.Current}");
+
+    await Task.Delay(millisec);//.ConfigureAwait(ConfigureAwaitOptions.ForceYielding);    
+
+    Console.WriteLine($"DoDelay2: {Environment.CurrentManagedThreadId}: SyncCtx: {SynchronizationContext.Current}");
+
+    //throw new InvalidOperationException("ABCD");
 }
 
 class MySyncronizationContext : SynchronizationContext
---
+++ Task12.CustomAwaiter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+var me = new MyAwaiter();
+
+Console.WriteLine($"Run {Environment.CurrentManagedThreadId}");
+
+_ = Task.Run(() =>
+{
+    Thread.Sleep(1000);
+    Console.WriteLine($"SetCompleted {Environment.CurrentManagedThreadId}");
+    me.SetCompleted();
+});
+
+await me;
+
+Console.WriteLine($"After await {Environment.CurrentManagedThreadId}");
+
+class MyAwaiter:INotifyCompletion
+{
+    public bool IsCompleted => _completed;
+
+    public MyAwaiter GetAwaiter() => this;
+
+    public void GetResult() { }
+
+    public void OnCompleted(Action continuation)
+    {
+        _continuation = continuation;
+    }
+
+    public void SetCompleted()
+    {
+        _completed = true;
+        _continuation();
+    }
+
+    private Action _continuation;
+    private bool _completed;
+}

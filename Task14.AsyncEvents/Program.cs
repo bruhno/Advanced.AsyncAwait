e68b2f5b446b63baf0f7666ac9736c159b9ec090
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-03-17 12:26:04 +0300

    AsyncEvents multcast

--- Task14.AsyncEvents/Program.cs
+++ Task14.AsyncEvents/Program.cs
@@ -8,28 +8,60 @@ class Program
     {
     }
 
-    public static event AsyncEventHandler<ValueEventArgsAsync> OnProcess = default!;
+
+    public static event AsyncEventHandler<ValueEventArgsAsync> OnProcess
+    {
+        add
+        {
+            _handlers.Add(value);
+        }
+        remove
+        {
+            _handlers.Remove(value);
+        }
+    }
 
     static async Task Main(string[] args)
     {
         OnProcess += async (e, a) =>
         {
-            await Task.Delay(100);
+            await Task.Delay(5000);
             Console.WriteLine("1");
         };
 
         OnProcess += async (e, a) =>
         {
-            await Task.Delay(10000);
+            await Task.Delay(200);
             Console.WriteLine("2");
         };
 
+        OnProcess += async (e, a) =>
+        {
+            await Task.Delay(300);
+            Console.WriteLine("3");
+        };
 
-        await OnProcess.Invoke(null!, null!);
-        await OnProcess.Invoke(null!, null!);
-
-
+        await InvokeOnProcess(null!, null!);
+        await InvokeOnProcess(null!, null!);
 
         Console.WriteLine("Finish");
+
     }
+
+    private static Task InvokeOnProcess(object sender, ValueEventArgsAsync args)
+    {
+        Task task = null!;
+
+        foreach (var handler in _handlers)
+        {
+            task = (task is null)
+                ? Task.Run(() => handler(sender, args))
+                : task.ContinueWith(_ => handler(sender, args)).Unwrap();
+        }
+
+        return task;
+    }
+
+    private static List<AsyncEventHandler<ValueEventArgsAsync>> _handlers = [];
+    //private static AsyncEventHandler<ValueEventArgsAsync> _onProcess = default!;
 }

2bdacc855bca5f12c8a1b767e235dccd0a40d5a0
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-03-28 13:50:51 +0300

    19. SyncContext

---
+++ Task19.SynchronizationContext/Program.cs
@@ -0,0 +1,20 @@
+﻿SynchronizationContext.SetSynchronizationContext(new MySynchronizationContext());
+
+Console.WriteLine($"App start, Thread:{Environment.CurrentManagedThreadId}");
+var t = Task.Delay(1000);
+await t;
+Console.WriteLine($"Context - {SynchronizationContext.Current is not null}");
+Console.WriteLine($"After await:{Environment.CurrentManagedThreadId}");
+
+
+class MySynchronizationContext : SynchronizationContext
+{
+    public override void Post(SendOrPostCallback d, object? state)
+    {
+        base.Post(s =>
+        {
+            SetSynchronizationContext(this);
+            d(s);
+        }, state);
+    }
+}
